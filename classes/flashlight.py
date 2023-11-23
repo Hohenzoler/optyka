4212bbbd49580a1c707e7c7638d976cd4a71a27d
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2023-11-23 21:23:32 +0100

    add light to the flashlight

--- classes/flashlight.py
+++ classes/flashlight.py
@@ -1,4 +1,5 @@
 import pygame
+from classes import light
 class Flashlight:  #creating a flash light class
     def __init__(self, game, x, y):
         self.game = game
@@ -12,7 +13,9 @@ class Flashlight:  #creating a flash light class
 
     def render(self): # basic drawing functions
         pygame.draw.rect(self.game.screen, "grey", self.rect)
+        light1 = light.Light(self.game, ([self.x + self.width, self.y+self.heigh//2], [self.game.width, self.y+self.heigh//2]), (255, 255,255))
 
     def drawoutline(self):
         self.game.screen.blit(self.transparent_surface, (self.x, self.y)) #draws a transparent outline of the flashlight at the mouse pos
+
 
--- main.py
+++ main.py
@@ -6,6 +6,6 @@ width = 1000
 height = 1000
 
 game=game.Game(width,height)
-light1=light.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
+# light1=light.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
 GUI = gui.GUI(game, width, height)
 game.loop()
