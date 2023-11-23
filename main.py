913c8792861db6ac140668992ea3a787c108a90b
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2023-11-23 17:56:19 +0100

    Przeorganizowalem kod lolka

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import classes
-game=classes.Game(500,500)
-light1=classes.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
+import game
+import light
+
+game=game.Game(1000,1000)
+light1=light.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
 game.loop()
