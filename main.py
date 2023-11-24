084596eb067d939a9b79739dbef28cacca1f37c2
artur <arturkummer08@gmail.com>
2023-11-24 17:54:56 +0100

    more ctrlaltl

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from gui import gui
 width = 1000
 height = 1000
 
-game=game.Game(width,height)
+game = game.Game(width, height)
 # light1=light.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
 GUI = gui.GUI(game, width, height)
 game.loop()
