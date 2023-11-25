cff4382f6910e89ac675774da8df63c0dc920900
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2023-11-25 11:21:37 +0100

    Game object (#2)
    * resizable
    
    * Revert "resizable"
    
    This reverts commit 4d09c968aa92b2f5a7d3875bcb3b1ccf0b649153.
    
    * render pass
    
    * name from heigh to height
    
    * def move, simpel def render, moved more things to game object
    
    * moved random things to class game object
    
    * def render in game object
    
    * mousepos not needed in class flashlight
    
    * moved all of render to game object
    
    * rotated outline
    
    * nothing important
    
    * rotating added, and changed rect to surface

--- classes/flashlight.py
+++ classes/flashlight.py
@@ -1,54 +1,70 @@
 import pygame
 from classes import light
 
+
 class GameObject:
 
-    def __init__(self, game, x, y, heigh, width):
+    def __init__(self, game, x, y, height, width, angle, color, islighting):
         self.game = game
         self.width = width
-        self.height = heigh
+        self.height = height
         self.x = x - self.width // 2  # adjusted so that when someone creates a flashlight it places in the correct spot
         self.y = y - self.height // 2
+        self.angle = angle
+        self.color = color
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-
-class Flashlight(GameObject):  # Inheriting from GameObject
-    def __init__(self, game, x, y):
-        super().__init__(game, x, y, 100, 200)  # Call the constructor of the parent class
-        self.transparent_surface = pygame.Surface((200, 100), pygame.SRCALPHA)
+        self.surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.surface.fill(self.color)
+        self.transparent_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         self.transparent_surface.fill((255, 255, 255, 50))  # last number is the alpha value (transparency)
         self.on = True
         self.selectedtrue = False
+        self.islighting = bool(islighting) # it is boolean, true, false or maybe
 
-    def render(self):  # basic drawing functions
-        mousepos = pygame.mouse.get_pos()
+    def render(self):
         if not self.selectedtrue:
-            pygame.draw.rect(self.game.screen, "grey", self.rect)
-            if self.on:
-                self.light = light.Light(self.game, ([self.x + self.width, self.y + self.height // 2], [self.game.width, self.y + self.height // 2]), (255, 255, 255))
-            elif not self.on:
-                self.light = None
+            rotated_surface = pygame.transform.rotate(self.surface, self.angle)
+            rotated_rect = rotated_surface.get_rect(center=self.rect.center)
+            self.game.screen.blit(rotated_surface, rotated_rect.topleft)
 
+            if self.islighting:
+                if self.on:
+                    light_start = (self.x + self.width, self.y + self.height // 2)
+                    light_end = (self.game.width, self.y + self.height // 2)
+                    self.light = light.Light(self.game, (light_start, light_end), "white")
+                elif not self.on:
+                    self.light = None
         else:
-            self.x = mousepos[0] - self.width//2
-            self.y = mousepos[1] - self.height // 2
-            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-            self.game.screen.blit(self.transparent_surface, (self.x, self.y))
+            self.move()
+
+    def move(self): #code for movimg object with mouse
+        self.mousepos = pygame.mouse.get_pos()
+        self.x = self.mousepos[0] - self.width // 2
+        self.y = self.mousepos[1] - self.height // 2
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.game.screen.blit(self.transparent_surface, (self.x, self.y))
 
     def drawoutline(self):
-        self.game.screen.blit(self.transparent_surface, (self.x, self.y))  # draws a transparent outline of the flashlight at the mouse pos
+        rotated_surface = pygame.transform.rotate(self.transparent_surface, self.angle)
+        rotated_rect = rotated_surface.get_rect(center=self.rect.center)
+        self.game.screen.blit(rotated_surface, rotated_rect.topleft) # draws a transparent outline of the flashlight at the mouse pos
 
-    def checkifclicked(self, mousepos):
+    def checkifclicked(self, mousepos): #checks if object is clicked
         if self.rect.collidepoint(mousepos):
             if self.on == 1:
                 self.on = 0
             else:
                 self.on = 1
 
-    def selected(self, mousepos):
-        if self.rect.collidepoint(mousepos) and self.selectedtrue == False:
+    def selected(self, mousepos): #checks if object is selected
+        if self.rect.collidepoint(mousepos) and self.selectedtrue is False:
             self.selectedtrue = True
 
-        elif self.rect.collidepoint(mousepos) and self.selectedtrue == True:
+        elif self.rect.collidepoint(mousepos) and self.selectedtrue is True:
             self.selectedtrue = False
+
+
+class Flashlight(GameObject):  # Inheriting from GameObject
+    def __init__(self, game, x, y):
+        super().__init__(game, x, y, 100, 200, 20, "red", True)  # Call the constructor of the parent class
 
