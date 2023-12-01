88cd16dbe039200f355794ed830e0109bffb4601
artur <arturkummer08@gmail.com>
2023-12-01 20:10:33 +0100

    app icon

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from classes import game
 from gui import gui
+import pygame
 
 width = 1000
 height = 700
+programIcon = pygame.image.load('images/torch.png')
 
+pygame.display.set_icon(programIcon)
 game = game.Game(width, height)
 # light1=light.Light(game,[[0,0],[100,10],[200,300]],(255,255,255))
 GUI = gui.GUI(game, width, height)
