bfe150b5be975d8b278cf92e4fae286a6befb262
artur <arturkummer08@gmail.com>
2023-12-10 20:19:17 +0100

    nvm i broke smt
    This reverts commit ef80515722383439243b0c34f2055d3383c9ae36.

--- classes/light.py
+++ classes/light.py
@@ -58,8 +58,6 @@ class Light:
                             self.points.append((self.vx, self.vy))
                             object_angle=-object.angle/360*2*math.pi
                             print(object_angle)
-                            if object_angle == 0:
-                                break
                             self.bend(object_angle)
     def update_vp(self):
 
