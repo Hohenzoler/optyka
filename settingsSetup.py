b182e98fb1d52ded1f3a9bc896874c652602341e
artur <arturkummer08@gmail.com>
2024-01-07 17:14:11 +0100

    settingsSetup.py update

--- settingsSetup.py
+++ settingsSetup.py
@@ -1,7 +1,7 @@
 import os.path
 import json
 
-settings = {'WIDTH': 1000, 'HEIGHT': 700, 'FULLSCREEN': 'OFF', 'HOPBAR_POSITION': 'bottom'}
+settings = {'WIDTH': 1000, 'HEIGHT': 700, 'FULLSCREEN': 'OFF', 'HOTBAR_POSITION': 'bottom', 'VSYNC': 'OFF'}
 
 def start(s=settings):
     if os.path.exists('settings.json'):
