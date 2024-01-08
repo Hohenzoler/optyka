d6cfd019729d10e76068abfb775b9db0f5737bfd
artur <arturkummer08@gmail.com>
2024-01-08 22:16:09 +0100

    checks refresh rate of device, and matches same fps

--- classes/fps.py
+++ classes/fps.py
@@ -1,2 +1,13 @@
+import win32api
+
+device_info = win32api.EnumDisplayDevices()
+
+def refresh_rate(device):
+    settings = win32api.EnumDisplaySettings(device.DeviceName, -1)
+    return settings.DisplayFrequency
+
+print(refresh_rate(device_info))
+
+
 def return_fps():
-    return 165
+    return refresh_rate(device_info)
