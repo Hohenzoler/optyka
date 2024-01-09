d6195208ae102bc0640189f001ea167c7637deee
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2024-01-09 20:30:26 +0100

    add simple bin

---
+++ classes/bin.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Bin:
+    def __init__(self, game):
+        self.game = game
+        self.x = self.game.width - 150
+        self.y = self.game.height - 200
+
+        self.rect = pygame.Rect(self.x, self.y, 100, 100)
+
+        self.game.objects.append(self)
+
+    def render(self):
+        pygame.draw.rect(self.game.screen, 'grey', self.rect)
+
+    def checkCollision(self, obj):
+        if obj.rect.colliderect(self.rect):
+            self.game.objects.remove(obj)
