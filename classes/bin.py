1503ea48621b6621d0942052c5d6d572486d8fa7
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2024-01-11 18:04:44 +0100

    finished bin render positioning

--- classes/bin.py
+++ classes/bin.py
@@ -14,7 +14,7 @@ class Bin:
 
     def render(self):
         self.game.screen.blit(self.bin_img, self.rect)
-        pygame.draw.rect(self.game.screen, 'white', self.rect, 2)
+        # pygame.draw.rect(self.game.screen, 'white', self.rect, 2)
 
     def checkCollision(self, obj):
         if obj.rect.colliderect(self.rect) and isinstance(obj, go.GameObject):
@@ -25,8 +25,19 @@ class Bin:
         self.load_parameters()
 
     def load_parameters(self):
+        settings = self.game.settings
+
         self.x = self.game.width - self.game.width // 10
         self.y = self.game.height - self.game.height // 4
+
+        if settings['HOTBAR_POSITION'] != 'bottom':
+            self.y = self.game.height - self.game.height // 7
+            if settings['HOTBAR_POSITION'] != 'right':
+                self.x = self.game.width - self.game.width // 10
+
+            else:
+                self.x = self.game.width - self.game.width // 5
+
 
         self.rect_w = self.game.width // 14
         self.rect_h = self.game.height // 8
