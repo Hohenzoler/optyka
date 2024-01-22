447dd60d667481d10456b33db1e851f1b94ef240
artur <arturkummer08@gmail.com>
2024-01-22 18:34:23 +0100

    rlly buggy sliders

---
+++ classes/slider.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Slider:
+    def __init__(self, x, y, width, height, min_val, max_val, initial_val):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.min_val = min_val
+        self.max_val = max_val
+        self.val = initial_val
+        self.slider_pos = (x + (initial_val - min_val) / (max_val - min_val) * width, y)
+
+    def render(self, screen):
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
+        pygame.draw.circle(screen, (255, 0, 0), (int(self.slider_pos[0]), int(self.slider_pos[1])), 10)
+
+    def check_if_clicked(self, pos):
+        if self.rect.collidepoint(pos):
+            self.slider_pos = (pos[0], self.slider_pos[1])
+            self.val = self.min_val + (self.slider_pos[0] - self.rect.x) / self.rect.width * (self.max_val - self.min_val)
+            return True
+        return False
+
+    def get_value(self):
+        return int(self.val)
--- gui/gui_main.py
+++ gui/gui_main.py
@@ -1,6 +1,6 @@
 import pygame
 from gui import button
-from classes import bin
+from classes import bin, slider
 
 
 class GUI:
@@ -9,6 +9,10 @@ class GUI:
         self.width = self.game.width
         self.height = self.game.height // 10
         self.position = self.game.settings['HOTBAR_POSITION']
+
+        self.red_slider = slider.Slider(50, 50, 200, 20, 0, 255, 255)
+        self.green_slider = slider.Slider(50, 100, 200, 20, 0, 255, 255)
+        self.blue_slider = slider.Slider(50, 150, 200, 20, 0, 255, 255)
 
         if self.position == 'bottom':
             self.rect = pygame.Rect(0, self.height*10 - self.height, self.width, self.height)
@@ -33,11 +37,23 @@ class GUI:
         pygame.draw.rect(self.game.screen, (100, 100, 100), self.rect)
         for button in self.buttons: #renders buttons
             button.render()
-
+        self.red_slider.render(self.game.screen)
+        self.green_slider.render(self.game.screen)
+        self.blue_slider.render(self.game.screen)
 
     def checkifclicked(self, mousepos):
         for button in self.buttons:
             button.checkifclicked(mousepos)
+
+        if self.red_slider.check_if_clicked(mousepos) or self.green_slider.check_if_clicked(
+                mousepos) or self.blue_slider.check_if_clicked(mousepos):
+            r = self.red_slider.get_value()
+            g = self.green_slider.get_value()
+            b = self.blue_slider.get_value()
+            # Assuming the light color is stored in self.game.current_flashlight.color
+            if self.game.current_flashlight:
+                self.game.current_flashlight.color = (r, g, b)
+
 
     def load_settings(self):
         self.width = self.game.width
