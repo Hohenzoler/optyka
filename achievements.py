bb25e5dd8898ad1c6c9655f7615abd6ca42a1e80
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2024-01-25 16:05:14 +0100

    fps lower than 10 achievement

---
+++ achievements.py
@@ -0,0 +1,68 @@
+import sqlite3
+from classes import game
+import time
+
+class Achievements:
+    def __init__(self, game):
+        self.conn = sqlite3.connect('achievements.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS achievements (
+                achievement_name TEXT PRIMARY KEY,
+                unlocked INTEGER,
+                rarity TEXT
+            )
+        """)
+        self.conn.commit()
+        self.game = game
+        self.start_time = time.time()
+
+
+        # Define the dictionary of achievements
+        self.achievements_dict = {
+            "first_flashlight_placed": "common",
+            "first_mirror_placed": "common",
+            "BIN": "uncommon",
+            "back and forth": "epic",
+            "need for nasa": "epic",
+            "complex_numbers": "legendary",
+        }
+
+    def unlock_achievement(self, achievement_name):
+        # Check if the achievement exists in the dictionary
+        if achievement_name not in self.achievements_dict:
+            print(f"Error: Achievement '{achievement_name}' does not exist.")
+            return
+
+        rarity = self.achievements_dict[achievement_name]
+        self.cursor.execute("""
+            INSERT OR REPLACE INTO achievements VALUES (?, ?, ?)
+        """, (achievement_name, 1, rarity))
+        self.conn.commit()
+        print(f"Achievement Unlocked: {achievement_name}, Rarity: {rarity}")
+
+    def is_achievement_unlocked(self, achievement_name):
+        self.cursor.execute("""
+            SELECT unlocked FROM achievements WHERE achievement_name = ?
+        """, (achievement_name,))
+        result = self.cursor.fetchone()
+        return result[0] if result else False
+
+    def get_achievement_rarity(self, achievement_name):
+        self.cursor.execute("""
+            SELECT rarity FROM achievements WHERE achievement_name = ?
+        """, (achievement_name,))
+        result = self.cursor.fetchone()
+        return result[0] if result else "unknown"
+
+    def handle_achievement_unlocked(self, achievement_name):
+        if self.is_achievement_unlocked(achievement_name):
+            print(f"Achievement already unlocked: {achievement_name}")
+            return
+
+        self.unlock_achievement(achievement_name)
+
+    def fps_achievements(self):
+        if time.time()>self.start_time+5:
+            if int(game.Game.return_fps(self.game)) < 10:
+                self.handle_achievement_unlocked("need for nasa")
---
+++ game.py
@@ -0,0 +1,174 @@
+import pygame
+from gui import gui_main as gui
+from gui import settings_screen, polygonDrawing
+from classes import gameobjects
+import settingsSetup
+from classes import fps
+from classes import bin, images
+from classes.achievements import Achievements
+
+isDrawingModeOn = False
+class Game:
+    """
+    The main game class that handles the game loop, events, rendering and settings.
+    """
+    def __init__(self):
+        """
+        Initializes the game with settings, pygame, objects, and other necessary attributes.
+        """
+        self.settings = settingsSetup.load_settings()  # Load game settings
+        self.width = self.settings['WIDTH']  # Game width
+        self.height = self.settings['HEIGHT']  # Game height
+        self.font = pygame.font.Font(None, self.height//20)  # Font for displaying FPS
+        self.objects = []  # List of game objects
+        pygame.init()  # Initialize pygame
+        # Set up the game display based on settings
+        if self.settings['FULLSCREEN'] == 'ON':
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN, vsync=0)
+        else:
+            self.screen = pygame.display.set_mode((self.width, self.height), vsync=0)
+        self.run = True  # Game loop control
+        self.fps = fps.return_fps()  # Frames per second
+        self.tick = int((1 / self.fps) * 1000)  # Time per frame in milliseconds
+        self.mousepos = None  # Mouse position which will be updated every time the left mouse button is clicked
+        self.rightclickedmousepos = None  # Right click mouse position
+        self.r = False  # Used for mouse wheel events
+        self.current_flashlight = None  # Current flashlight object
+        self.mode = 'default'  # Current game mode
+        self.executed_command = 'default'  # Last executed command
+        self.clock = pygame.time.Clock()  # Pygame clock for controlling FPS
+        pygame.mouse.set_visible(False)  # Hide the default mouse cursor
+        self.cursor_img = images.bad_coursor  # Custom cursor image
+        self.cursor_img_rect = self.cursor_img.get_rect()  # Rectangle for the custom cursor image
+        self.pen_img = images.pen
+        self.pen_img_rect = self.pen_img.get_rect()
+        self.achievements = Achievements(self)  # Achievements object
+
+        self.p = False #used for properties windows for gameobjects
+
+        self.selected_object = None
+
+
+    def return_fps(self):
+        return self.displayFPS()
+
+    def events(self):
+        """
+        Handles all the pygame events.
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                self.run = False
+            if self.mode == 'default':
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.mousepos = event.pos  # when the left button is clicked the position is saved to self.mousepos
+                    if isDrawingModeOn:
+                        optyka.gui.polygonDrawing.addPoint(self.mousepos)
+
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                    self.rightclickedmousepos = event.pos
+                if event.type == pygame.MOUSEWHEEL:
+                    if event.y > 0:
+                        self.r = 5
+                    if event.y < 0:
+                        self.r = -5
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                    self.p = True
+            elif self.mode == 'settings':
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    for object in self.objects:
+                        if type(object) == settings_screen.Settings_screen:
+                            object.checkevent(event.pos)
+
+    def update(self):
+        """
+        Updates the game display and controls the game FPS.
+        """
+        pygame.display.update()
+        self.clock.tick(self.fps)
+
+    def render(self):
+        """
+        Renders all the game objects and the custom cursor.
+        """
+        if self.mode == 'default':
+            sorted_objects = sorted(self.objects, key=lambda obj: getattr(obj, 'layer', 0))
+            for object in sorted_objects:
+                if type(self.mousepos) is tuple:
+                    if type(object) is gui.GUI:
+                        object.checkifclicked(self.mousepos)
+                    if issubclass(type(object), gameobjects.GameObject):
+                        object.checkifclicked(self.mousepos)
+                if type(self.rightclickedmousepos) is tuple:
+                    if issubclass(type(object), gameobjects.GameObject):
+                        object.selected(self.rightclickedmousepos)
+
+
+                object.render()
+                if type(object) != bin.Bin:
+                    for bin_2 in self.objects:
+                        if type(bin_2) == bin.Bin:
+                            bin_2.checkCollision(object)
+                            break
+            if isDrawingModeOn:
+                optyka.gui.polygonDrawing.renderDots()
+        elif self.mode == 'settings':
+            if self.executed_command != 'settings':
+                self.settings_screen = settings_screen.Settings_screen(self)
+                self.settings_screen.render()
+                self.executed_command = 'settings'
+            else:
+                self.settings_screen.render()
+        elif self.mode == 'load_new_settings':
+            self.objects.remove(self.settings_screen)
+            self.settings_screen = None
+            self.mode = 'default'
+            self.settings = settingsSetup.load_settings()
+            self.width = self.settings['WIDTH']
+            self.height = self.settings['HEIGHT']
+            if self.settings['FULLSCREEN'] == 'ON':
+                self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
+            else:
+                self.screen = pygame.display.set_mode((self.width, self.height))
+            for object in self.objects:
+                if type(object) == gui.GUI or type(object) == bin.Bin:
+                    object.load_settings()
+            self.executed_command = 'default'
+        self.cursor_img_rect.center = pygame.mouse.get_pos()  # update position
+        if isDrawingModeOn:
+            self.screen.blit(self.pen_img, self.cursor_img_rect)
+        else:
+            self.screen.blit(self.cursor_img, self.cursor_img_rect)  # draw the cursor
+        self.displayFPS()
+
+    def background(self):
+        """
+        Fills the game display with black color.
+        """
+        self.screen.fill((0, 0, 0))
+
+    def displayFPS(self):
+        """
+        Displays the current FPS on the game display.
+        """
+        fps = self.clock.get_fps()
+        fps_text = self.font.render(f"FPS: {int(fps)}", True, "white")
+        self.screen.blit(fps_text, (10, 10))
+        return fps
+
+
+
+    def loop(self):
+        """
+        The main game loop.
+        """
+        while self.run:
+            self.background()
+            self.render()
+            self.update()
+            self.mousepos = None  # resets self.mouspos
+            self.rightclickedmousepos = None
+            self.p = False
+            self.events()
+            self.achievements.fps_achievements()
