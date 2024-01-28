6907cf7aabf83d0310871900c42f6a79500efcfa
artur <arturkummer08@gmail.com>
2024-01-28 14:51:12 +0100

    made particles fade out

--- classes/parkinson.py
+++ classes/parkinson.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Particle:
-    def __init__(self, x, y, vx, vy, lifespan, size, red, green, blue):
+    def __init__(self, x, y, vx, vy, lifespan, size, red, green, blue, alpha):
         self.x = x
         self.y = y
         self.vx = vx
@@ -11,21 +11,26 @@ class Particle:
         self.red = red
         self.green = green
         self.blue = blue
+        self.alpha = alpha
 
     def update(self):
         self.x += self.vx
         self.y += self.vy
+        if self.alpha > 0:
+            self.alpha -= 1
         self.lifespan -= 2
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (self.red, self.green, self.blue), (self.x, self.y), self.size)
-
+        surface = pygame.Surface((self.size * 2, self.size * 2), pygame.SRCALPHA)  # Create a surface
+        pygame.draw.circle(surface, (self.red, self.green, self.blue, self.alpha), (self.size, self.size),
+                           self.size)  # Draw the particle on the surface
+        screen.blit(surface, (self.x - self.size, self.y - self.size))
 class UnityParticleSystem:
     def __init__(self):
         self.particles = []
 
-    def add_particle(self, x, y, vx, vy, lifespan, size, red, green, blue):
-        self.particles.append(Particle(x, y, vx, vy, lifespan, size, red, green, blue))
+    def add_particle(self, x, y, vx, vy, lifespan, size, red, green, blue, alpha):
+        self.particles.append(Particle(x, y, vx, vy, lifespan, size, red, green, blue, alpha))
 
     def update(self):
         for particle in self.particles:
