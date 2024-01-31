7aff5321b89c8cd4831dbf74a417d69963469614
artur <arturkummer08@gmail.com>
2024-01-31 21:06:02 +0100

    bugfix

--- gui/button_animation.py
+++ gui/button_animation.py
@@ -5,4 +5,7 @@ class ButtonAnimation:
 
     def animate(self):
         self.button.rect.y += (self.target_position - self.button.rect.y) * 0.1
-        self.button.textRect.center = (self.button.rect[0] + (self.button.rect[2] // 2), self.button.rect[1] + (self.button.rect[3] // 2))
+        try:
+            self.button.textRect.center = (self.button.rect[0] + (self.button.rect[2] // 2), self.button.rect[1] + (self.button.rect[3] // 2))
+        except:
+            pass
