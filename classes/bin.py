95520e51e6665d59bd052649a7d9f135701c5fd6
rutra4 <asda@asda>
2024-02-14 09:40:12 +0100

    BIN

--- classes/bin.py
+++ classes/bin.py
@@ -54,7 +54,7 @@ class Bin:
                 else:
                     rgb = (255, 255, 255)
             for i in range(random.randint(60, 300)):
-                self.particle_system.add_particle(self.particle_center_x, self.particle_center_y, random.uniform(-1.5, 1.5)*obj.scale_factor, random.uniform(-1.5, 1.5)*obj.scale_factor, 220, random.randint(1, 7)*obj.scale_factor, random.randint(rgb[0]//2, rgb[0]), random.randint(rgb[1]//2, rgb[1]), random.randint(rgb[2]//2, rgb[2]), 220, 'square')
+                self.particle_system.add_particle(self.particle_center_x, self.particle_center_y, random.uniform(-1.5, 1.5)*obj.scale_factor, random.uniform(-1.5, 1.5)*obj.scale_factor, 220, random.randint(1, 7)*obj.scale_factor, random.randint(rgb[0]//2, rgb[0]), random.randint(rgb[1]//2, rgb[1]), random.randint(rgb[2]//2, rgb[2]), 220, random.choice(['square','circle']))
             self.game.objects.remove(obj)
             sounds.destroy_sound()
             achievements.Achievements.handle_achievement_unlocked(self.achievements, "BIN")
