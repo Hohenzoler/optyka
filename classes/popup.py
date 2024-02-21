a8343b30fda099c3e6ab13a972bcb51890e20518
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2024-02-21 09:24:14 +0100

    Update popup.py

--- classes/popup.py
+++ classes/popup.py
@@ -10,13 +10,27 @@ class Popup:
         self.font = self.game.font
 
 
-    def render_achievement(self, achievement, x_offset, y_offset):
+    def render_achievement(self, achievement, rarity, x_offset, y_offset):
         self.font = self.game.font
+        self.rarity = rarity
         achievement_name = achievement
         text_color = (255, 255, 255)
         text_surface = self.font.render(f"{achievement_name}", True, text_color)
 
-        rect_width = self.width // 2 - 100
+        if rarity == 'common':
+            color = (163, 163, 163)
+        elif rarity == 'uncommon':
+            color = (10, 145, 6)
+        elif rarity == 'rare':
+            color = (15, 109, 163)
+        elif rarity == 'epic':
+            color = (97, 37, 143)
+        elif rarity == 'legendary':
+            color = (222, 182, 24)
+        else:
+            color = (50, 50, 50)
+
+        rect_width = self.width - 100
         rect_height = 50
 
         textRect = pygame.Rect(x_offset, y_offset, rect_width, rect_height)
@@ -27,7 +41,7 @@ class Popup:
 
         text_surface_rect = text_surface.get_rect(center=(center_x, center_y))
 
-        pygame.draw.rect(self.game.screen, (0, 0, 0), textRect.inflate(10, 10), 0, 5)
+        pygame.draw.rect(self.game.screen, color, textRect.inflate(10, 10), 0, 5)
         self.game.screen.blit(text_surface, text_surface_rect)
         return y_offset + rect_height + 20
 
