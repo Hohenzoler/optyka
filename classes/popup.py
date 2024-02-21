be02f6a2286d8549d0bf80053cefde1b1839d268
rutra_arch <arturkummer08@gmail.com>
2024-02-21 18:29:15 +0100

    added achievement unlocked text

--- classes/popup.py
+++ classes/popup.py
@@ -24,11 +24,11 @@ class Popup:
         return color
 
     def render_achievement(self, achievement, rarity, x_offset, y_offset):
-        self.font = self.game.font
+        self.smol_font = pygame.font.Font(font.Font, 15)
+        self.normal_font = pygame.font.Font(font.Font, 30)
         self.rarity = rarity
         achievement_name = achievement
         text_color = (255, 255, 255)
-        text_surface = self.font.render(f"{achievement_name}", True, text_color)
 
         color = self.rarity_color(self.rarity)
 
@@ -41,9 +41,16 @@ class Popup:
         center_x = x_offset + rect_width // 2
         center_y = y_offset + rect_height // 2
 
-        text_surface_rect = text_surface.get_rect(center=(center_x, center_y))
-
         pygame.draw.rect(self.game.screen, color, textRect.inflate(10, 10), 0, 5)
+
+        unlocked_text_surface = self.normal_font.render("Achievement Unlocked", True, text_color)
+        unlocked_text_rect = unlocked_text_surface.get_rect(center=(center_x, center_y -10))
+        self.game.screen.blit(unlocked_text_surface, unlocked_text_rect)
+
+        text_surface = self.smol_font.render(f"{achievement_name}", True, text_color)
+        text_surface_rect = text_surface.get_rect(center=(center_x, center_y+15))
+        self.game.screen.blit(text_surface, text_surface_rect)
+
         self.game.screen.blit(text_surface, text_surface_rect)
         return y_offset + rect_height + 20
 
