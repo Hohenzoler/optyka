932a4ee0f1ecf99c62946df99f68452ff16a89d3
rutra_arch <arturkummer08@gmail.com>
2024-02-21 19:32:30 +0100

    metsej sounds

--- classes/sounds.py
+++ classes/sounds.py
@@ -2,24 +2,24 @@ import pygame
 
 
 def selected_sound():
-    selected_sound = pygame.mixer.Sound('sounds/select.wav')
+    selected_sound = pygame.mixer.Sound('sounds/metsej/metsej_1.wav')
     selected_sound.play()
     pass
 
 
 def placed_sound():
-    placed_sound = pygame.mixer.Sound('sounds/place.wav')
+    placed_sound = pygame.mixer.Sound('sounds/metsej/metsej_2.wav')
     placed_sound.play()
     pass
 
 
 def clicked_sound():
-    clicked_sound = pygame.mixer.Sound('sounds/select.wav')
+    clicked_sound = pygame.mixer.Sound('sounds/metsej/metsej_1.wav')
     clicked_sound.play()
     pass
 
 def laser_sound():
-    laser_sound = pygame.mixer.Sound('sounds/place.wav')
+    laser_sound = pygame.mixer.Sound('sounds/metsej/metsej_2.wav')
     laser_sound.play()
     pass
 
