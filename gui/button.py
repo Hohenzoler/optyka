38830dafa0c10bc7f233f56de4c2ec3c02d5c42f
lolekszcz <lolo.szczygiel@outlook.com>
2024-02-28 09:24:32 +0100

    collisions

--- gui/button.py
+++ gui/button.py
@@ -209,7 +209,7 @@ class Button:
                     obj = gameobjects.Mirror(self.game, [(mousepos[0] - 100, mousepos[1] - 50),
                                                          (mousepos[0] + 100, mousepos[1] - 50),
                                                          (mousepos[0] + 100, mousepos[1] + 50),
-                                                         (mousepos[0] - 100, mousepos[1] + 50)], (200, 200, 200), 0, 0, 0)
+                                                         (mousepos[0] - 100, mousepos[1] + 50)], (200, 200, 200), 0.0001, 0, 0)
                     self.game.achievements.handle_achievement_unlocked("is it... me?")
 
                 elif self.number == 2:
