63d8fa3564f43e4001a355e6877180b609d0ce81
artur <arturkummer08@gmail.com>
2024-07-10 14:56:27 +0200

    random changes, deleted usles things

--- gui/ModifyParameters.py
+++ gui/ModifyParameters.py
@@ -49,7 +49,7 @@ class Parameters:
         if type(object) == gameobjects.Lens:
             l -= 2
 
-        self.root.geometry(f'300x{60*l}')
+        self.root.geometry(f'400x{100*l}')
 
         self.root.mainloop()
 
