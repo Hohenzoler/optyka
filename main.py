55049889777d7415b1e664b718a7617a4a20fb41
artur <arturkummer08@gmail.com>
2024-07-17 17:42:02 +0200

    why was it beta?

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ log_file = f"logs/{datetime.now().strftime('%Y-%m-%d')}.log"
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-version = '1.1.1-enhanced-beta'
+version = '1.1.1-enhanced'
 
 def new_game(save, preset):
     try:
